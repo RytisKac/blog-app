03b973cf9d411a58e6e585f57f372e3ea6bfe79f
RytisKac <rytiskacinskis@gmail.com>
2023-11-05 15:42:00 +0200

    Fix redirect after login

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <div class="text-center pt-24 pb-12 bg-gray-100">
-    <h1 class="max-w-5xl mx-auto mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">{{$title}}</h1>
+    <h1 class="max-w-5xl mx-auto mb-4 leading-snug font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-red-700 to-orange-500">{{$title}}</h1>
     <div>
         <p class="max-w-5xl mx-auto mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48">{{$slot}}</p>
     </div>
